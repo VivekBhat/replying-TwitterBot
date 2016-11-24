ca03c839c64755dd5b1359425ce49cfde54f18ca
Vivek Bhat <vivekbhat@live.com>
2016-11-24 09:03:05 -0500

    removed files

--- config.js
+++
@@ -1,8 +0,0 @@
- 
- module.exports = 
- {
-  consumer_key:         'tSDE3v0t100p6GsbA2b72A9AS',
-  consumer_secret:      'jx2ag37dq9nDE1E9m6KpXiGmqaXvjgryO2QEPdILR333eEkcyU',
-  access_token:         '783024655401713664-95pPOjppfoIiWWjSBjLN32chMNJ4ZwP',
-  access_token_secret:  'R2O2dFqLaaopIa3VniVmrHWbke1L2CCxt4qFWnCRyX7em'
-}
